88d169bff41c38d46dd6b491c96d75a1cf3f7473
Ersapta Aristo <aristoersapta@gmail.com>
2023-12-09 18:09:48 +0700

    [main] define BookScraperItem Item Class

--- book_scraper/items.py
+++ book_scraper/items.py
@@ -1,12 +1,9 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Item, Field
 
 
-class BookScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class BookScraperItem(Item):
+    image_path_url = Field()
+    price = Field()
+    title = Field()
+    in_stock = Field()
+    rating = Field()
