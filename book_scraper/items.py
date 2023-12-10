b106478efd7cb20427bf3ecd83e785c014bf005a
Ersapta Aristo <aristoersapta@gmail.com>
2023-12-10 15:22:16 +0700

    [main] add get rating to convert pronounce to number

--- book_scraper/items.py
+++ book_scraper/items.py
@@ -4,6 +4,18 @@ from itemloaders.processors import MapCompose, TakeFirst
 def get_price(txt):
     return float(txt.replace('£', ''))
 
+def get_rating(txt):
+    if(txt == 'One'):
+        return 1
+    elif(txt == 'Two'):
+        return 2
+    elif(txt == 'Three'):
+        return 3
+    elif(txt == 'Four'):
+        return 4
+    elif(txt == 'Five'):
+        return 5
+
 class BookScraperItem(Item):
     image_path_url = Field(
         output_processor=TakeFirst(),
@@ -20,5 +32,6 @@ class BookScraperItem(Item):
         output_processor=TakeFirst(),
     )
     rating = Field(
+        input_processor=MapCompose(get_rating),
         output_processor=TakeFirst(),
     )
